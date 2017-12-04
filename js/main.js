16c27cfec33b35501481bca2a1030a8f10037c6a
Garrett Rasko-Martinis <garrettrasko-martinis@Garretts-Air.fios-router.home>
2017-12-04 03:36:24 -0500

    used objects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{	
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{	
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}			
+];
 var cardsInPlay = []
 
 var checkForMatch = function(){
@@ -10,9 +31,11 @@ var checkForMatch = function(){
 }
 
 var flipCard = function(cardId){
-	cardsInPlay.push(cards[cardId])
-	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId].rank)
+	console.log("User flipped " + cards[cardId].rank)
 	checkForMatch();
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)	
 }
 flipCard(0);
 flipCard(2);
